5cb58da75069d9cfcf0a404782ebcd1e427756b3
Dmitriy Kondakov <dikon.25@yandex.ru>
2024-04-21 17:56:12 +0300

    fist try test for main

--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
 from main import BooksCollector
 
 class TestBooksCollector:
-
      def test_add_new_book_add_two_books(self,collector):
 
         collector.add_new_book('Гордость и предубеждение и зомби')
